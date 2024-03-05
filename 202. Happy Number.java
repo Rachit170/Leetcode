423f6b206d11c4301b22d7b42d84eba677b99969
Rachit Rawat <148671943+Rachit170@users.noreply.github.com>
2024-03-05 18:05:47 +0530

    Create 202. Happy Number.java

---
+++ 202. Happy Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int sum=0;
+        while(sum!=1 && sum!=4){
+            sum=0;
+                while(n!=0){
+                    sum+=(n%10)*(n%10);
+                    n/=10;
+                }
+            n=sum;
+        }
+        return sum==1;
+    }
+}
