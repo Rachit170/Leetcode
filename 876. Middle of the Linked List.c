181d18956a0e82aeb112342d6ffe00e1cdad4017
Rachit Rawat <148671943+Rachit170@users.noreply.github.com>
2024-03-21 08:47:11 +0530

    Create 876. Middle of the Linked List.c

---
+++ 876. Middle of the Linked List.c
@@ -0,0 +1,9 @@
+struct ListNode* middleNode(struct ListNode* head) {
+    struct ListNode *fast=head;
+    struct ListNode* slow=head;
+    while(fast!=NULL && fast->next!=NULL){
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    return slow ;
+}
