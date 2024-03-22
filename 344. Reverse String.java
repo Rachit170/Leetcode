4e496602fa3c9907ac9b96155691dfb11257705b
Rachit Rawat <148671943+Rachit170@users.noreply.github.com>
2024-03-22 21:38:19 +0530

    Update and rename 344. Reverse String.cpp to 344. Reverse String.java

--- 344. Reverse String.java
+++ 344. Reverse String.java
@@ -1,11 +1,10 @@
 class Solution {
-public:
-    void reverseString(vector<char>& s) {
+    public void reverseString(char[] s) {
         char temp;
-        for(int i=0,j=s.size()-1;i<j;i++,j--){
+        for(int i=0,j=s.length-1;i<j;i++,j--){
             temp=s[i];
             s[i]=s[j];
             s[j]=temp;
         }
     }
-};
+}
